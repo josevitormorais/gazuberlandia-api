5446dac2860eadc59c2eebc86d3a7eae80441083
jose vitor morais <josevitormorais7@gmail.com>
2021-09-15 13:53:34 -0300

    Update user.go

--- handler/user.go
+++ handler/user.go
@@ -11,27 +11,6 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-/* HANDLER -> qualquer estrutura que embedding http.Handler ou implementa os metodos de http.Handler
- */
-
-/*
-	ROUTER: -> interessante é usar uma unica instancia injetar essa instancia em funções
-	que fazem o registro dos paths. porque dessa forma se tiver muitos dominos
-	como: user, customer, auth etc. cada um deles terá uma função(constructor,factory)
-	que recebe a instancia do router e registra os paths especificos.
-
-	essa instancia entao deve ser criada em um função q tenha um nivel mais alto,
-	como a main ou alguma função que tenha visão e consiga chamar todas que dependem da
-	injeção do router.
-
-	esse router tem que estar contido dentro de uma estrutura, como Server ou Application.
-*/
-
-/*
-	SERVICES/BUSINESS-LOGIC: -> precisa da injeção de dependencias como db, logger.
-	1º Criar uma struct Global, como Application ou Server. fazer o emmbed dos serviços
-	nessa struct, que precisaram ser instanciados e injetado as instancias de db, logger.
-*/
 
 func (h *HttpServer) RegisterUserRoutes(r *chi.Mux) {
 	r.Get("/user/{userId}", h.HandlerFindUserById)
